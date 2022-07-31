7a0f282e9fddd419385bcfb4e14c9135cc3d866d
SageCreations <sagecreations.ec@gmail.com>
2022-07-31 16:12:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# UE5_Practice_Project
+
+Developed with Unreal Engine 5
---
+++ Source/UE5_Practice_Project/UE5_Practice_ProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5_Practice_ProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_PRACTICE_PROJECT_API AUE5_Practice_ProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
