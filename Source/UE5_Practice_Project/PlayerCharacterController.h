34ee1ddcdb89a5857250d9e4e3e83c46534e4099
SageCreations <sagecreations.ec@gmail.com>
2022-08-07 00:00:39 -0500

    Revert "Merge pull request #3 from SageCreations/Edward"
    This reverts commit 681d62c1fbcda779c0a45da69d5da75d98b53a6f, reversing
    changes made to cdf48048765b0724fbc86988d8fcd7a6e6a587bf.

--- Source/UE5_Practice_Project/PlayerCharacterController.h
+++ Source/UE5_Practice_Project/PlayerCharacterController.h
@@ -27,7 +27,6 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	void Attack();
-	void AttackCoolDown(float DeltaTime);
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animations")
 	UAnimationAsset* AttackAnimationAsset;
